bbdc0f477054dc007cfa7a2ff9b8acf7958e8808
Omer Eisenberg <omereis@yahoo.com>
2023-02-07 10:39:12 +0200

    Initial Commit

---
+++ GMix/main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace GMix {
+	public partial class frmMain : Form {
+		RequestSocket m_requester = null;
+		public frmMain() {
+			InitializeComponent();
+			m_requester = new RequestSocket();
+			m_requester.Connect("tcp://localhost:5555");
+		}
+	}
+}
